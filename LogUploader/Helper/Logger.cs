3703037b3a4ca776f0cbe5fb514b8008128199d2
ProfBits <53108880+ProfBits@users.noreply.github.com>
2021-01-10 10:56:16 +0100

    Added OS and build to log, added manifest with sopport from win 7 to 10

--- LogUploader/Helper/Logger.cs
+++ LogUploader/Helper/Logger.cs
@@ -77,13 +77,101 @@ namespace LogUploader.Helper
 
         private static void WriteHeader(string version)
         {
+            string build;
+#if DEBUG
+            build = "DEBUG";
+#elif BETA
+            build = "BETA";
+#elif ALPHA
+            build = "ALPHA";
+#else
+            build = "Release";
+#endif
+
             var text = $@"### LogUploader Log
 ### Version:  {version}
+### Build:    {build}
+### OS:       {GetOSString()}
 ### Date:     {DateTime.Now:yyyy'-'MM'-'dd HH':'mm}
 ### LogLevel: {LogLevel}
 
 ";
             File.AppendAllText(LogFile, text, Encoding.UTF8);
+        }
+
+        private static string GetOSString()
+        {
+            OperatingSystem os = Environment.OSVersion;
+            Version vs = os.Version;
+
+            string operatingSystem = "";
+
+            if (os.Platform == PlatformID.Win32Windows)
+            {
+                switch (vs.Minor)
+                {
+                    case 0:
+                        operatingSystem = "95";
+                        break;
+                    case 10:
+                        if (vs.Revision.ToString() == "2222A")
+                            operatingSystem = "98SE";
+                        else
+                            operatingSystem = "98";
+                        break;
+                    case 90:
+                        operatingSystem = "Me";
+                        break;
+                    default:
+                        break;
+                }
+            }
+            else if (os.Platform == PlatformID.Win32NT)
+            {
+                switch (vs.Major)
+                {
+                    case 3:
+                        operatingSystem = "NT 3.51";
+                        break;
+                    case 4:
+                        operatingSystem = "NT 4.0";
+                        break;
+                    case 5:
+                        if (vs.Minor == 0)
+                            operatingSystem = "2000";
+                        else
+                            operatingSystem = "XP";
+                        break;
+                    case 6:
+                        if (vs.Minor == 0)
+                            operatingSystem = "Vista";
+                        else if (vs.Minor == 1)
+                            operatingSystem = "7";
+                        else if (vs.Minor == 2)
+                            operatingSystem = "8";
+                        else
+                            operatingSystem = "8.1";
+                        break;
+                    case 10:
+                        operatingSystem = "10";
+                        break;
+                    default:
+                        break;
+                }
+            }
+            if (os.Platform == PlatformID.Win32Windows || os.Platform == PlatformID.Win32NT)
+            {
+                if (operatingSystem != "")
+                {
+                    operatingSystem = "Windows " + operatingSystem;
+                    if (os.ServicePack != "")
+                    {
+                        operatingSystem += " " + os.ServicePack;
+                    }
+                }
+                return operatingSystem;
+            }
+            return $"{os.Platform} {os.Version}";
         }
 
         private static void Log(string prefix, string msg)
