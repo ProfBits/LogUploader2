94dd07bdbe4b2537ea332e47818fb59edd6470a4
ProfBits <53108880+ProfBits@users.noreply.github.com>
2021-04-15 08:20:39 +0200

    Add a factory interface for a GUI element

---
+++ LogUploader.Interfaces/Factroies/UiFactory.cs
@@ -0,0 +1,29 @@
+﻿using LogUploader.Data;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LogUploader.Interfaces.Factroies
+{
+    public static class UiFactory
+    {
+        public delegate Form LoadingBarUiFactory(string TaskName, Action<CancellationToken, Action<Delegate>, IProgress<ProgressMessage>> task);
+        private static LoadingBarUiFactory _loadingBarUiFactory = null;
+        
+        public static void SetLoadingBarUiFactory(LoadingBarUiFactory creator)
+        {
+            _loadingBarUiFactory = creator ?? throw new ArgumentNullException("Factory delegate cannot be null.");
+        }
+
+        public static Form GetLoadingBarUi(string TaskName, Action<CancellationToken, Action<Delegate>, IProgress<ProgressMessage>> task)
+        {
+            return (_loadingBarUiFactory ?? throw new InvalidOperationException("Factory not setup currently"))(TaskName, task);
+        }
+
+    }
+}
