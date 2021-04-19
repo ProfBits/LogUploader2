d4c5a700d5fc67f57ea5ba21022ced5314880e64
ProfBits <53108880+ProfBits@users.noreply.github.com>
2021-04-19 14:42:36 +0200

    Add abstraction layer for web I/O via HttpClient

---
+++ LogUploader.Interfaces/Wrapper/IWebIO.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading;
+using System.IO;
+using LogUploader.Tools.Settings;
+
+namespace LogUploader.Interfaces.Wrapper
+{
+    public interface IWebIO : IDisposable
+    {
+        TimeSpan Timeout { get; set; }
+        IProxySettings ProxySettings { get; }
+
+        Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken);
+        Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, HttpCompletionOption completionOption, CancellationToken cancellationToken);
+
+    }
+}
---
+++ LogUploader.Interfaces/Wrapper/WebIO.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+using LogUploader.Tools.Settings;
+
+namespace LogUploader.Interfaces.Wrapper
+{
+    public static class WebIOFactory
+    {
+        //TODO singelton HttpsClient
+
+        internal static IWebIOFactory Factory { private get; set; } = new HttpClientWebIOFactory();
+
+        public static IWebIO Create(IProxySettings settings) => Factory.Create(settings);
+
+    }
+
+    internal interface IWebIOFactory
+    {
+        IWebIO Create(IProxySettings settings);
+    }
+
+    internal sealed class HttpClientWebIOFactory : IWebIOFactory
+    {
+        internal HttpClientWebIOFactory() { }
+
+        public IWebIO Create(IProxySettings settings)
+        {
+            return new WebIO(settings);
+        }
+
+        private sealed class WebIO : IWebIO
+        {
+            private bool disposedValue;
+
+            private readonly HttpClient Client = new HttpClient();
+
+            internal WebIO(IProxySettings proxySettings)
+            {
+                ProxySettings = proxySettings;
+            }
+
+            public TimeSpan Timeout { get => Client.Timeout; set => Client.Timeout = value; }
+            public IProxySettings ProxySettings { get; }
+
+            public async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+            {
+                return await Client.SendAsync(request, cancellationToken);
+            }
+
+            public async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, HttpCompletionOption completionOption, CancellationToken cancellationToken)
+            {
+                return await Client.SendAsync(request, completionOption, cancellationToken);
+            }
+
+            private void Dispose(bool disposing)
+            {
+                if (!disposedValue)
+                {
+                    if (disposing)
+                    {
+                        Client.Dispose();
+                    }
+                    disposedValue = true;
+                }
+            }
+            public void Dispose()
+            {
+                Dispose(disposing: true);
+                GC.SuppressFinalize(this);
+            }
+        }
+    }
+
+}
