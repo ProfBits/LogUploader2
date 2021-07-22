dd27474a3abd5a6e8bdf10444cabc3bd3e9430a2
ProfBits <53108880+ProfBits@users.noreply.github.com>
2021-07-22 13:31:46 +0200

    Create a synchronos implementation of the IProgress<T> interface
    This was necessary because of the default Progress<T> implementation uses tasks in the
    background making it impossible to test the progress reporting in a unit test. With the
    sychronos implementation it is easily possible to test the progress reporting aswell.

---
+++ LogUploader.Test/SynchronProgress.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogUploader.Test
+{
+    internal class SynchronProgress<T> : IProgress<T>
+    {
+        private readonly Action<T> handler;
+
+        public SynchronProgress(Action<T> handler)
+        {
+            this.handler = handler ?? throw new ArgumentNullException("Handler cannot be null");
+        }
+
+        public void Report(T value)
+        {
+            handler(value);
+        }
+    }
+}
