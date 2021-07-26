78ec35efe17146d792fde062955e79422ec6edf5
ProfBits <53108880+ProfBits@users.noreply.github.com>
2021-07-26 19:43:27 +0200

    Refactored PerWingWithClasses to improve readiablility

--- LogUploader.Tools.Discord/DiscordPostGen/PerWingWithClasses.cs
+++ LogUploader.Tools.Discord/DiscordPostGen/PerWingWithClasses.cs
@@ -15,39 +15,58 @@ namespace LogUploader.Tools.Discord
         {
             if (Settings.OnlyPostUploaded && string.IsNullOrWhiteSpace(log.Link))
                 return null;
+            string name = GenerateName(log);
+
+            string value = GenerateDateAndDuration(log);
+            value += GenerateLink(log);
+
+            if (log.DataCorrected)
+            {
+                value += GenerateClassIcons(log);
+            }
+
+            return new Field(name, value, true);
+        }
+
+        private static string GenerateClassIcons(ICachedLog log)
+        {
+            string value = "";
+            var subGroups = log.PlayersNew.GroupBy(p => p.Group).OrderBy(g => g.Key);
+            foreach (var grop in subGroups)
+            {
+                value += "\n";
+                foreach (var player in grop.OrderBy(p => ((Profession)p.Profession).Name))
+                {
+                    value += player.Profession.Emote;
+                }
+            }
+
+            return value;
+        }
+
+        private static string GenerateLink(ICachedLog log)
+        {
+            if (!string.IsNullOrWhiteSpace(log.Link))
+                return $"[dps.report]({log.Link})";
+            return Language.Data.MiscDiscordPostGenNoLink;
+        }
+
+        private static string GenerateDateAndDuration(ICachedLog log)
+        {
+            string value = log.Date.ToString("HH':'mm");
+            if (log.DataCorrected)
+                value += $" - {log.Duration.Minutes}m {log.Duration.Seconds}s";
+            return value + "\n";
+        }
+
+        private static string GenerateName(ICachedLog log)
+        {
             string name = log.Succsess ? MiscData.EmoteRaidKill : MiscData.EmoteRaidWipe;
             name += Boss.GetByID(log.BossID).DiscordEmote;
             name += " " + log.BossName;
             if (log.IsCM)
                 name += " CM";
-
-            string value = "";
-
-            value += log.Date.ToString("HH':'mm");
-            if (log.DataCorrected)
-                value += $" - {log.Duration.Minutes}m {log.Duration.Seconds}s";
-            value += "\n";
-
-
-            if (!string.IsNullOrWhiteSpace(log.Link))
-                value += $"[dps.report]({log.Link})";
-            else
-                value += Language.Data.MiscDiscordPostGenNoLink;
-
-            if (log.DataCorrected)
-            {
-                var subGroups = log.PlayersNew.GroupBy(p => p.Group).OrderBy(g => g.Key);
-                foreach (var grop in subGroups)
-                {
-                    value += "\n";
-                    foreach (var player in grop.OrderBy(p => ((Profession)p.Profession).Name))
-                    {
-                        value += player.Profession.Emote;
-                    }
-                }
-            }
-
-            return new Field(name, value, true);
+            return name;
         }
     }
 }
