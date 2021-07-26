8b916ffcc97c50a233d8c57c924b0094415486f6
ProfBits <53108880+ProfBits@users.noreply.github.com>
2021-07-26 10:52:13 +0200

    Seperate the language test classes into multible files

---
+++ LogUploader.Test/Localisation/AbstractXmlLocalisationTest.cs
@@ -0,0 +1,24 @@
+﻿
+using LogUploader.Localisation;
+
+namespace LogUploader.Test.Localisation
+{
+    public abstract class AbstractXmlLocalisationTest : AbstractLocalisationTest
+    {
+        internal abstract string FileName { get; }
+
+        internal override ILanguage GetLanguage()
+        {
+            return Language.LoadFromXML(FileName);
+        }
+    }
+    public class EnglishXmlLocalisationTest : AbstractXmlLocalisationTest
+    {
+        internal override string FileName { get => "English"; }
+    }
+
+    public class GermanXmlLocalisationTest : AbstractXmlLocalisationTest
+    {
+        internal override string FileName { get => "German"; }
+    }
+}
---
+++ LogUploader.Test/Localisation/XmlLocalisationTest.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+using NUnit.Framework;
+
+using LogUploader.Localisation;
+using System.IO;
+
+namespace LogUploader.Test.Localisation
+{
+    public class XmlLocalisationTest
+    {
+        [Test]
+        public void AllXmlFilesHaveATest()
+        {
+            IEnumerable<string> xmlFilesCovered = null;
+            Assert.Multiple(() => xmlFilesCovered = GetTestedXMLFilesFromCrrentDomain().Distinct());
+
+            var available = Directory.GetFiles(Language.LANG_XML_FOLDER_PATH + $@"\{Language.DATA_FOLDER}\", "*.xml")
+                .Select(path => Path.GetFileNameWithoutExtension(path))
+                .OrderBy(fileName => fileName)
+                .ToList();
+
+            foreach (var langFile in available)
+            {
+                CollectionAssert.Contains(xmlFilesCovered, langFile);
+            }
+        }
+
+        private static List<string> GetTestedXMLFilesFromCrrentDomain()
+        {
+            List<string> xmlFilesCovered = new List<string>();
+
+            foreach (Assembly asm in AppDomain.CurrentDomain.GetAssemblies())
+            {
+                foreach (Type type in asm.GetTypes())
+                {
+                    if (type.IsSubclassOf(typeof(AbstractXmlLocalisationTest)) && !type.IsAbstract)
+                    {
+                        if (type.GetConstructor(Type.EmptyTypes) != null)
+                        {
+                            AbstractXmlLocalisationTest testClass = (AbstractXmlLocalisationTest)asm.CreateInstance(type.FullName);
+                            xmlFilesCovered.Add(testClass.FileName);
+                        }
+                        else
+                            Assert.Fail($"Concrete subtype \"{type.FullName}\" of \"{typeof(AbstractXmlLocalisationTest).FullName}\" shold provide a default constructor");
+                    }
+                }
+            }
+
+            return xmlFilesCovered;
+        }
+    }
+}
