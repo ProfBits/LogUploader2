cad206c211a59f93e6850bfbe660765ca9322dd9
ProfBits <53108880+ProfBits@users.noreply.github.com>
2021-07-29 11:02:02 +0200

    Implement ProfessionDataBuilder
    Implemented the ProfessionDataBuilder with its test.
    The ParseJson methode is writte like a transaktion,
    if any error occures nothing will be changed on the ProfessionRepository.

--- LogUploader.Data/Builder/ProfessionDataBuilder.cs
+++ LogUploader.Data/Builder/ProfessionDataBuilder.cs
@@ -1,19 +1,105 @@
 ﻿using System;
+using System.Collections;
+using System.Linq;
+
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 using LogUploader.Data.Repositories;
+using LogUploader.Tools;
+using System.Collections.Generic;
 
 namespace LogUploader.Data
 {
     public static class ProfessionDataBuilder
     {
-        public static void ParseJson(string json)
+        private const string ROOT_TAG = "Professions";
+        private const string TAG_ID = "ID";
+        private const string TAG_NAME_EN = "NameEN";
+        private const string TAG_NAME_DE = "NameDE";
+        private const string TAG_ICON_PATH = "IconPath";
+        private const string TAG_EMOTE = "Emote";
+        private const string TAG_ROP_ID = "RaidOrgaPlusID";
+        private const string TAG_ROP_ABBREVIATION = "RaidOrgaPlusAbbreviation";
+
+        public static void ParseJson(string json) => ParseJson(json, StaticData.ProfessionRepository);
+
+        internal static void ParseJson(string json, ProfessionRepository professionRepo)
         {
-            throw new NotImplementedException();
+            if (json is null) throw new ArgumentNullException(nameof(json), "The JSON string cannot be null");
+            if (professionRepo is null) throw new ArgumentNullException(nameof(professionRepo), "ProfessionRepository cannot be null");
+
+            JArray parsedData = ParseString(json);
+            IEnumerable<Profession> professions = LoadProfessionsInToRepo(parsedData);
+            StoreProfessions(professions, professionRepo);
         }
 
-        internal static void ParseJson(string json, ProfessionRepository testProfessionRepo)
+        private static void StoreProfessions(IEnumerable<Profession> professions, ProfessionRepository professionRepo)
         {
-            throw new NotImplementedException();
+            List<eProfession> added = new List<eProfession>();
+
+            try
+            {
+                foreach (var profession in professions)
+                {
+                    professionRepo.Add(profession);
+                    added.Add(profession.ProfessionEnum);
+                }
+            }
+            catch (ArgumentException ex)
+            {
+                foreach (var prof in added)
+                {
+                    professionRepo.Remove(prof);
+                }
+                throw ex;
+            }
+        }
+
+        private static IEnumerable<Profession> LoadProfessionsInToRepo(JArray professionsData)
+        {
+            foreach (JObject professionData in professionsData)
+            {
+                yield return ParseProfession(professionData);
+            }
+        }
+
+        private static JArray ParseString(string json)
+        {
+            try
+            {
+                JArray professions = (JArray)JObject.Parse(json)[ROOT_TAG];
+                if (professions is null) throw new ArgumentException("Provided JSON does not contian required root object " + ROOT_TAG, nameof(json));
+                return professions;
+            }
+            catch (JsonReaderException readerEx)
+            {
+                ArgumentException jsonEx = new ArgumentException("Provided JSON has invalid json format", nameof(json), readerEx);
+                Tools.Logging.Logger.LogException(jsonEx);
+                throw jsonEx;
+            }
+        }
+
+        private static Profession ParseProfession(JObject professionData)
+        {
+            try
+            {
+                int id = (int)professionData[TAG_ID];
+                string nameEN = (string)professionData[TAG_NAME_EN];
+                string nameDE = (string)professionData[TAG_NAME_DE];
+                string iconPath = (string)professionData[TAG_ICON_PATH];
+                string emote = (string)professionData[TAG_EMOTE];
+                int raidOrgaPlusID = (int)professionData[TAG_ROP_ID];
+                string raidOrgaPlusAbbreviation = (string)professionData[TAG_ROP_ABBREVIATION];
+                return new Profession(GP.IntToEnum<eProfession>(id), nameEN, nameDE, iconPath, emote, raidOrgaPlusID, raidOrgaPlusAbbreviation);
+            }
+            catch (JsonReaderException readerEx)
+            {
+                ArgumentException jsonEx = new ArgumentException($"Provided profession JSON object has invalid format or structure" +
+                    $"at L{readerEx.LineNumber}:{readerEx.LinePosition}.\n" + professionData.ToString(), nameof(professionData), readerEx);
+                Tools.Logging.Logger.LogException(jsonEx);
+                throw jsonEx;
+            }
         }
     }
 }
--- LogUploader.Test/Data/ProfessionDataBuilderTest.cs
+++ LogUploader.Test/Data/ProfessionDataBuilderTest.cs
@@ -7,16 +7,162 @@ using System.Threading.Tasks;
 using LogUploader.Data.Repositories;
 using LogUploader.Data;
 using NUnit.Framework;
+using Newtonsoft.Json;
 
 namespace LogUploader.Test.Data
 {
     class ProfessionDataBuilderTest
     {
-        [Test, Ignore("just a placeholder will be implmentat after repository")]
-        public void ParseProfessionData()
+        private string ProfessionIconPath { get => TestSetup.GetPathToTestFiles("static", "profIcon.png"); }
+        private string SmallValidTestJson { get => $@"
+{{
+  ""Professions"": [
+    {{
+      ""ID"": 0,
+      ""NameEN"": ""Unknown"",
+      ""NameDE"": ""Unbekannt"",
+      ""IconPath"": ""{JsonEscapeString(ProfessionIconPath)}"",
+      ""Emote"": "":ghost:"",
+      ""RaidOrgaPlusID"": 0,
+      ""RaidOrgaPlusAbbreviation"": ""XXX""
+    }},
+    {{
+      ""ID"": 2,
+      ""NameEN"": ""Guardian"",
+      ""NameDE"": ""Wächter"",
+      ""IconPath"": ""{JsonEscapeString(ProfessionIconPath)}"",
+      ""Emote"": ""<:gdn:999999999999999999>"",
+      ""RaidOrgaPlusID"": 8,
+      ""RaidOrgaPlusAbbreviation"": ""Gdn""
+    }}
+  ]
+}}"; }
+
+        private string SmallInvalidThroughDupelicateTestJson { get => $@"
+{{
+  ""Professions"": [
+    {{
+      ""ID"": 2,
+      ""NameEN"": ""Guardian"",
+      ""NameDE"": ""Wächter"",
+      ""IconPath"": ""{JsonEscapeString(ProfessionIconPath)}"",
+      ""Emote"": ""<:gdn:999999999999999999>"",
+      ""RaidOrgaPlusID"": 8,
+      ""RaidOrgaPlusAbbreviation"": ""Gdn""
+    }},
+    {{
+      ""ID"": 2,
+      ""NameEN"": ""Guardian"",
+      ""NameDE"": ""Wächter"",
+      ""IconPath"": ""{JsonEscapeString(ProfessionIconPath)}"",
+      ""Emote"": ""<:gdn:999999999999999999>"",
+      ""RaidOrgaPlusID"": 8,
+      ""RaidOrgaPlusAbbreviation"": ""Gdn""
+    }}
+  ]
+}}"; }
+
+        private string SmallInvalidThroughValueTestJson { get => $@"
+{{
+  ""Professions"": [
+    {{
+      ""ID"": 2,
+      ""NameEN"": null,
+      ""NameDE"": ""Wächter"",
+      ""IconPath"": ""{JsonEscapeString(ProfessionIconPath)}"",
+      ""Emote"": ""<:gdn:999999999999999999>"",
+      ""RaidOrgaPlusID"": 8,
+      ""RaidOrgaPlusAbbreviation"": ""Gdn""
+    }}
+  ]
+}}"; }
+
+        private static string JsonEscapeString(string str)
         {
-            ProfessionRepository testProfessionRepo = new TestProfessionRepo();
-            ProfessionDataBuilder.ParseJson("json", testProfessionRepo);
+            string escaped = JsonConvert.ToString(str);
+            //Remove encasing double quotes
+            return escaped.Substring(1, escaped.Length - 2);
+        }
+
+        [Test]
+        public void ParseProfessionDataNoErrorTest()
+        {
+            ProfessionRepository testProfessionRepo = new ProfessionRepository();
+            Assert.DoesNotThrow(() => ProfessionDataBuilder.ParseJson(SmallValidTestJson, testProfessionRepo));
+        }
+
+        [Test]
+        public void ParseProfessionDataCorrectTest()
+        {
+            TestProfessionRepo testProfessionRepo = new TestProfessionRepo();
+            ProfessionDataBuilder.ParseJson(SmallValidTestJson, testProfessionRepo);
+            Assert.That(testProfessionRepo.Count, Is.EqualTo(2));
+            ValidateProfessionCorrectlyParsed(testProfessionRepo, eProfession.Guardian, "Guardian", "Wächter", "Gdn", 8, "<:gdn:999999999999999999>", ProfessionIconPath);
+            ValidateProfessionCorrectlyParsed(testProfessionRepo, eProfession.Unknown, "Unknown", "Unbekannt", "XXX", 0, ":ghost:", ProfessionIconPath);
+        }
+
+        private void ValidateProfessionCorrectlyParsed(ProfessionProvider professionProvider, eProfession profession, string nameEN, string nameDE, string abbreviation, int ropId, string emote, string professionIconPath)
+        {
+            Profession prof = professionProvider.Get(profession);
+
+            Assert.That(prof.ProfessionEnum, Is.EqualTo(profession));
+            Assert.That(prof.NameEN, Is.EqualTo(nameEN));
+            Assert.That(prof.NameDE, Is.EqualTo(nameDE));
+            Assert.That(prof.Abbreviation, Is.EqualTo(abbreviation));
+            Assert.That(prof.RaidOrgaPlusID, Is.EqualTo(ropId));
+            Assert.That(prof.Emote, Is.EqualTo(emote));
+            Assert.That(prof.IconPath, Is.EqualTo(professionIconPath));
+        }
+
+        [Test]
+        public void ParseProfessionNullParameterTest()
+        {
+            ProfessionRepository testProfessionRepo = new ProfessionRepository();
+            TestHelper.ValidateArugumentException(Assert.Catch<ArgumentNullException>(() => ProfessionDataBuilder.ParseJson(null, testProfessionRepo)));
+            TestHelper.ValidateArugumentException(Assert.Catch<ArgumentNullException>(() => ProfessionDataBuilder.ParseJson(SmallValidTestJson, null)));
+            TestHelper.ValidateArugumentException(Assert.Catch<ArgumentNullException>(() => ProfessionDataBuilder.ParseJson(null, null)));
+        }
+
+        [Test]
+        public void ParseProfessionInvalidJsonTest()
+        {
+            ProfessionRepository testProfessionRepo = new ProfessionRepository();
+            TestHelper.ValidateArugumentException(Assert.Catch<ArgumentException>(() => ProfessionDataBuilder.ParseJson("", testProfessionRepo)));
+            TestHelper.ValidateArugumentException(Assert.Catch<ArgumentException>(() => ProfessionDataBuilder.ParseJson("this is non valid json:really not", testProfessionRepo)));
+            TestHelper.ValidateArugumentException(Assert.Catch<ArgumentException>(() => ProfessionDataBuilder.ParseJson(@"{""some"":""other JSON""}", testProfessionRepo)));
+            TestHelper.ValidateArugumentException(Assert.Catch<ArgumentException>(() => ProfessionDataBuilder.ParseJson(SmallInvalidThroughValueTestJson, testProfessionRepo)));
+            TestHelper.ValidateArugumentException(Assert.Catch<ArgumentException>(() => ProfessionDataBuilder.ParseJson(SmallInvalidThroughDupelicateTestJson, testProfessionRepo)));
+        }
+
+
+        [Test]
+        public void ParseProfessionJsonOnErrorAddsNothingTest()
+        {
+            TestProfessionRepo testProfessionRepo = new TestProfessionRepo();
+            const eProfession prof = eProfession.Scourge;
+            testProfessionRepo.Add(new Profession(prof, "Scourge", "Scourge", ProfessionIconPath, ":emote:", 99, "scg"));
+            
+            Assert.Catch<ArgumentException>(() => ProfessionDataBuilder.ParseJson("", testProfessionRepo));
+            Assert.That(testProfessionRepo.Count, Is.EqualTo(1), "There should have nothing been added or removed with empty string");
+            Assert.That(testProfessionRepo.Get(prof), Is.Not.Null);
+
+            Assert.Catch<ArgumentException>(() => ProfessionDataBuilder.ParseJson("this is non valid json:really not", testProfessionRepo));
+            Assert.That(testProfessionRepo.Count, Is.EqualTo(1), "There should have nothing been added or removed with ivalid json");
+            Assert.That(testProfessionRepo.Get(prof), Is.Not.Null);
+
+            Assert.Catch<ArgumentException>(() => ProfessionDataBuilder.ParseJson(@"{""some"":""other JSON""}", testProfessionRepo));
+            Assert.That(testProfessionRepo.Count, Is.EqualTo(1), "There should have nothing been added or removed with diffrent json");
+            Assert.That(testProfessionRepo.Get(prof), Is.Not.Null);
+
+            Assert.Catch<ArgumentException>(() => ProfessionDataBuilder.ParseJson(SmallInvalidThroughValueTestJson, testProfessionRepo));
+            Assert.That(testProfessionRepo.Count, Is.EqualTo(1), "There should have nothing been added or removed with SmallInvalidThroughValueTestJson");
+            Assert.That(testProfessionRepo.Get(prof), Is.Not.Null);
+            Assert.Catch<ArgumentException>(() => testProfessionRepo.Get(eProfession.Guardian));
+
+            Assert.Catch<ArgumentException>(() => ProfessionDataBuilder.ParseJson(SmallInvalidThroughDupelicateTestJson, testProfessionRepo));
+            Assert.That(testProfessionRepo.Count, Is.EqualTo(1), "There should have nothing been added or removed with SmallInvalidThroughDupelicateTestJson");
+            Assert.That(testProfessionRepo.Get(prof), Is.Not.Null);
+            Assert.Catch<ArgumentException>(() => testProfessionRepo.Get(eProfession.Guardian));
         }
     }
 
@@ -24,7 +170,10 @@ namespace LogUploader.Test.Data
     {
         public TestProfessionRepo()
         {
+
         }
+
+        internal int Count { get => Professions.Count; }
     }
 
 }
