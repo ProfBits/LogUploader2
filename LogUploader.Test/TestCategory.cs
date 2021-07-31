e431aae6c5e53622e109d1f2c58ec5f2481537e3
ProfBits <53108880+ProfBits@users.noreply.github.com>
2021-07-31 12:57:38 +0200

    Add TestCategory class
    This TestCategory class is a central place for all test category names.

---
+++ LogUploader.Test/TestCategory.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogUploader.Test
+{
+    public static class TestCategory
+    {
+        public const string Regression = "Regression";
+    }
+}
