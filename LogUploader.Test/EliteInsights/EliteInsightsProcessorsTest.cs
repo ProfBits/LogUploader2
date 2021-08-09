08bceaaf57389f1ecaa212e6e73b2a456e7ebb03
ProfBits <53108880+ProfBits@users.noreply.github.com>
2021-08-09 18:52:06 +0200

    Create concept of EliteInsights Json processors
    IEiProcessor is a class wich taks the json output of Elite Insights fom a speciefied range of EiVersion and
    processes it into a Full log for further usage.
    
    This approche enable the developers the be backward and mostly forward compatible with the Elite
    Insights json parsing wile keeping the maintenence mostly side effect free since every version of EI
    could have a totally unique and indipenden parsing.
    
    In this first commit only the shells of the minimal backward supported EI verions are created.
    Implementation of the Parserrs will follow up in the next commits.
    
    Note:
    The tests are in the LongRunning category and marked Explicite to keep test time in ckeck.

---
+++ LogUploader.Test/EliteInsights/EliteInsightsProcessorsTest.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using NUnit.Framework;
+
+using LogUploader.Tools.EliteInsights.Json;
+using System.IO.Compression;
+using LogUploader.Interfaces;
+using Newtonsoft.Json.Linq;
+
+namespace LogUploader.Test.EliteInsights
+{
+    [Explicit]
+    [Category(TestCategory.LogRunning)]
+    internal abstract class AbstractEliteInsightsProcessorTest
+    {
+        private const string VALIDATE_FILE_EXT = ".validate.json";
+
+        protected abstract string TestJsonZipName { get; }
+        protected abstract string TestTempFolderName { get; }
+        internal string AbsolutePathToTestJsonsZip { get => TestSetup.GetPathToTestFiles("eiJsonZips", TestJsonZipName); }
+        internal string AbsolutePathToTestTempFolder { get => TestSetup.GetPathToTestFiles("temp", TestTempFolderName); }
+
+        protected abstract IEiProcessor GetProcesor();
+
+        [OneTimeSetUp]
+        public void OneTimeSetUp()
+        {
+            ZipFile.ExtractToDirectory(AbsolutePathToTestJsonsZip, AbsolutePathToTestTempFolder);
+        }
+
+        [Test]
+        public void CanProcessJsonsTest()
+        {
+            foreach (string file in System.IO.Directory.GetFiles(AbsolutePathToTestTempFolder, $"*{VALIDATE_FILE_EXT}"))
+            {
+                StringAssert.EndsWith(VALIDATE_FILE_EXT, file);
+                string dataFile = file.Substring(0, file.Length - VALIDATE_FILE_EXT.Length) + ".json";
+                FileAssert.Exists(dataFile);
+
+                LogFull actual = ProcessJson(dataFile);
+                LogFull expected = ProcessJson(file);
+                Validate(actual, expected);
+            }
+        }
+
+        private LogFull ProcessJson(string absPathToDataJson)
+        {
+            string json = LogUploader.Tools.JsonHandling.ReadJsonFile(absPathToDataJson);
+            JObject parsed = JObject.Parse(json);
+            return GetProcesor().Process(parsed);
+        }
+
+        private LogFull PrepeareValidation(string absPathToValidateJson)
+        {
+            string json = LogUploader.Tools.JsonHandling.ReadJsonFile(absPathToValidateJson);
+            JObject parsed = JObject.Parse(json);
+            return GetProcesor().Process(parsed);
+        }
+
+        protected virtual LogFull CreateValidation(JObject data)
+        {
+            Assert.Warn("CreateValidation not Implemented");
+            return null;
+        }
+
+        protected virtual void Validate(LogFull actual, LogFull exptected)
+        {
+            Assert.Warn("Validate not Implemented");
+        }
+    }
+
+    [Explicit]
+    [Category(TestCategory.LogRunning)]
+    internal class EiProcessor_LegacyTest : AbstractEliteInsightsProcessorTest
+    {
+        protected override string TestJsonZipName { get => "legacy.zip"; }
+        protected override string TestTempFolderName { get => "legacy"; }
+
+        protected override IEiProcessor GetProcesor()
+        {
+            return new EiProcessor_Legacy();
+        }
+    }
+
+    [Explicit]
+    [Category(TestCategory.LogRunning)]
+    internal class EiProcessor_2_29_0_0Test : AbstractEliteInsightsProcessorTest
+    {
+        protected override string TestJsonZipName { get => "2_29_0_0.zip"; }
+        protected override string TestTempFolderName { get => "2_29_0_0"; }
+
+        protected override IEiProcessor GetProcesor()
+        {
+            return new EiProcessor_2_29_0_0();
+        }
+    }
+
+    [Explicit]
+    [Category(TestCategory.LogRunning)]
+    internal class EiProcessor_2_33_0_0Test : AbstractEliteInsightsProcessorTest
+    {
+        protected override string TestJsonZipName { get => "2_33_0_0.zip"; }
+        protected override string TestTempFolderName { get => "2_33_0_0"; }
+
+        protected override IEiProcessor GetProcesor()
+        {
+            return new EiProcessor_2_33_0_0();
+        }
+    }
+
+    [Explicit]
+    [Category(TestCategory.LogRunning)]
+    internal class EiProcessor_2_35_0_0Test : AbstractEliteInsightsProcessorTest
+    {
+        protected override string TestJsonZipName { get => "2_35_0_0.zip"; }
+        protected override string TestTempFolderName { get => "2_35_0_0"; }
+
+        protected override IEiProcessor GetProcesor()
+        {
+            return new EiProcessor_2_35_0_0();
+        }
+    }
+}
---
+++ LogUploader.Tools.EliteInsights/Json/EliteInsightJsonProcessor.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+using Newtonsoft.Json.Linq;
+
+using LogUploader.Interfaces;
+
+namespace LogUploader.Tools.EliteInsights.Json
+{
+    public static class EliteInsightJsonProcessor
+    {
+        internal static readonly IEiProcessor[] Processors = new IEiProcessor[]
+        {
+            new EiProcessor_2_35_0_0(),
+            new EiProcessor_2_33_0_0(),
+            new EiProcessor_2_29_0_0(),
+            new EiProcessor_Legacy()
+        };
+
+        internal static IEiProcessor GetEiProcessor(Version version)
+        {
+            return Processors.FirstOrDefault(p => p.IsVersionSupported(version));
+        }
+
+        public static LogFull Process(string json, IProgress<double> progress, CancellationToken ct = default)
+        {
+            progress?.Report(0);
+            
+            JObject parsed = JObject.Parse(json);
+            
+            progress?.Report(0.5);
+            
+            string eiVersion = (string)parsed["eliteInsightsVersion"];
+            Version version = new Version(eiVersion);
+            IEiProcessor processor = GetEiProcessor(version);
+            
+            progress?.Report(0.6);
+            
+            LogFull result = processor.Process(parsed, new SynchronusProgress<double>(p => progress.Report((p * 0.39) + 0.6)));
+            
+            progress?.Report(0.99);
+            
+            parsed = null;
+            Task.Run(GC.Collect);
+            
+            progress?.Report(1);
+            
+            return result;
+        }
+    }
+
+    internal interface IEiProcessor
+    {
+        bool IsVersionSupported(Version version);
+        LogFull Process(JObject log, IProgress<double> progress = null);
+    }
+    internal abstract class AbstractEiProcessor : IEiProcessor
+    {
+        protected abstract Version MinVersion { get; }
+        protected virtual Version MaxVersion { get; } = new Version(int.MaxValue, int.MaxValue, int.MaxValue, int.MaxValue);
+
+        public virtual bool IsVersionSupported(Version version)
+        {
+            return MinVersion <= version && version < MaxVersion;
+        }
+        public abstract LogFull Process(JObject log, IProgress<double> progress = null);
+    }
+
+    internal class EiProcessor_2_35_0_0 : AbstractEiProcessor
+    {
+        /* 
+         * Supports EliteInsightVersion 2.35.0.0 onwards
+         * Noteable features:
+         *  - Added IsFake to JsonActor
+         *  - Added EnemyPlayer to JsonNPC
+         *  - Added FriendlyNPC to JsonPlayer
+         */
+        protected override Version MinVersion { get; } = new Version(2, 35, 0, 0);
+
+        public override LogFull Process(JObject log, IProgress<double> progress = null)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    internal class EiProcessor_2_33_0_0 : AbstractEiProcessor
+    {
+        /* 
+         * Supports EliteInsightVersion 2.33.0.0 onwards
+         * Noteable features:
+         *  - Added friendly NPC concept. Saul (on Deimos) and Desmina (on River of Souls) will now appear as such
+         */
+        protected override Version MinVersion { get; } = new Version(2, 33, 0, 0);
+        protected override Version MaxVersion { get; } = new Version(2, 35, 0, 0);
+
+        public override LogFull Process(JObject log, IProgress<double> progress = null)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    internal class EiProcessor_2_29_0_0 : AbstractEiProcessor
+    {
+        /* 
+         * Supports EliteInsightVersion 2.29.0.0 onwards
+         * Noteable features:
+         *  - Added support for breakbar damage events
+         */
+        protected override Version MinVersion { get; } = new Version(2, 29, 0, 0);
+        protected override Version MaxVersion { get; } = new Version(2, 33, 0, 0);
+
+        public override LogFull Process(JObject log, IProgress<double> progress = null)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    internal class EiProcessor_Legacy : AbstractEiProcessor
+    {
+        /* 
+         * Supports EliteInsightVersion older than 2.29.0.0
+         */
+        protected override Version MinVersion { get; } = new Version(0, 0, 0, 0);
+        protected override Version MaxVersion { get; } = new Version(2, 29, 0, 0);
+
+        public override LogFull Process(JObject log, IProgress<double> progress = null)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
