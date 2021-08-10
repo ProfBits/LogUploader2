9deeaa187bcc90d701baaf386eb7d13f332946d4
ProfBits <53108880+ProfBits@users.noreply.github.com>
2021-08-10 16:13:56 +0200

    Add LogBuff interface

---
+++ LogUploader.Interfaces/Interfaces/Log/LogBuffs.cs
@@ -0,0 +1,17 @@
+﻿namespace LogUploader.Interfaces
+{
+    public interface LogBuffs
+    {
+        float GroupQuickness { get; }
+        float GroupAlacrety { get; }
+        float SquadQuickness { get; }
+        float SquadAlacrety { get; }
+        float SquadMight { get; }
+        float SquadFury { get; }
+        float SquadProtection { get; }
+        float SquadRegeneration { get; }
+        float SquadBannerStrength { get; }
+        float SquadBannerTactics { get; }
+    }
+}
+
