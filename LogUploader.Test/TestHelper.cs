5a8854d96ac8e682f205507fdaeee640682efb25
ProfBits <53108880+ProfBits@users.noreply.github.com>
2021-09-26 22:51:45 +0200

    Add testhelper for validating progress reports

--- LogUploader.Test/TestHelper.cs
+++ LogUploader.Test/TestHelper.cs
@@ -8,6 +8,7 @@ using System.Threading.Tasks;
 using LogUploader.Data;
 
 using NUnit.Framework;
+using NUnit.Framework.Internal;
 
 namespace LogUploader.Test
 {
@@ -44,6 +45,19 @@ namespace LogUploader.Test
             {
                 if (type.IsSubclassOf(baseType) && !type.IsAbstract)
                     yield return type;
+            }
+        }
+
+        public static void ValidateProgressReports(List<double> progressReports)
+        {
+            Assert.That(progressReports, Is.Not.Null);
+            Assert.That(progressReports, Has.Count.GreaterThan(2));
+            Assert.That(progressReports.Min(), Is.EqualTo(0d));
+            Assert.That(progressReports.Max(), Is.EqualTo(1d));
+            if (progressReports.Zip(progressReports.Skip(1), (a, b) => a >= b).Any(e => e))
+            {
+                var firstError = progressReports.Zip(progressReports.Skip(1), (a, b) => (a, b)).First(e => e.a >= e.b);
+                Assert.Fail($"Progress sequence is not strctly ascending. Fist error is {firstError.a} before {firstError.b}");
             }
         }
 
@@ -152,6 +166,108 @@ namespace LogUploader.Test
             IProgress<double> progressInterseption = new SynchronProgress<double>(p => progress.Report(p));
             progress.Report(2);
             Assert.That(reported, Is.EqualTo(2));
+        }
+
+        [Test]
+        public void TestValidateProgressReportsNullParamTest()
+        {
+            AssertAssertFail(() => ValidateProgressReports(null));
+        }
+
+        [Test]
+        public void TestValidateProgressReportsCountTest()
+        {
+            AssertAssertFail(() => ValidateProgressReports(null));
+
+            List<double> emptyList = new List<double>();
+            AssertAssertFail(() => ValidateProgressReports(emptyList));
+
+            List<double> only2Values = new List<double>() { 0, 1 };
+            AssertAssertFail(() => ValidateProgressReports(only2Values));
+        }
+
+        [Test]
+        public void TestValidateProgressReportsRequireZeroTest()
+        {
+            List<double> req0 = new List<double>() { 0.5, 0.75, 1 };
+            AssertAssertFail(() => ValidateProgressReports(req0));
+        }
+
+        [Test]
+        public void TestValidateProgressReportsRequireOneTest()
+        {
+            List<double> req1 = new List<double>() { 0, 0.25, 0.5 };
+            AssertAssertFail(() => ValidateProgressReports(req1));
+        }
+
+        [Test]
+        public void TestValidateProgressReportsZeroToOneRangeTest()
+        {
+
+            List<double> tooSmall = new List<double>() { -1, 0, 0.5, 1 };
+            AssertAssertFail(() => ValidateProgressReports(tooSmall));
+            List<double> tooSmall2 = new List<double>() { double.MinValue, 0, 0.5, 1 };
+            AssertAssertFail(() => ValidateProgressReports(tooSmall2));
+
+            List<double> tooBig = new List<double>() { 0, 0.5, 1, 2 };
+            AssertAssertFail(() => ValidateProgressReports(tooBig));
+            List<double> tooBig2 = new List<double>() { 0, 0.5, 1, double.MaxValue };
+            AssertAssertFail(() => ValidateProgressReports(tooBig2));
+        }
+
+        [Test]
+        public void TestValidateProgressReportsOrderTest()
+        {
+            List<double> tooWrongOrder = new List<double>() { 1, 0.5, 0 };
+            AssertAssertFail(() => ValidateProgressReports(tooWrongOrder));
+            List<double> tooWrongOrder2 = new List<double>() { 0, 1, 0.5 };
+            AssertAssertFail(() => ValidateProgressReports(tooWrongOrder2));
+            List<double> tooWrongOrder3 = new List<double>() { 0, 0.5, 0.5, 1 };
+            AssertAssertFail(() => ValidateProgressReports(tooWrongOrder3));
+        }
+        
+        [Test]
+        public void TestValidateProgressReportsOnValidDataTest()
+        {
+            List<double> correct = new List<double>() { 0, 0.5, 1 };
+            Assert.That(SafelyCatchAnNUnitException(() => ValidateProgressReports(correct)), Is.Null);
+        }
+
+        private void AssertAssertFail(Action action, NUnit.Framework.Interfaces.TestStatus expectedTestStatus = NUnit.Framework.Interfaces.TestStatus.Failed)
+        {
+            Exception ex = SafelyCatchAnNUnitException(action);
+            if (ex is AssertionException assertEx)
+            {
+                Assert.That(assertEx.ResultState.Status, Is.EqualTo(expectedTestStatus));
+            }
+            else if (ex is null)
+            {
+                Assert.Fail($"Expected to catch an {nameof(AssertionException)}");
+            }
+            else
+            {
+                throw ex;
+            }
+        }
+
+        //Taken from https://github.com/nunit/nunit/issues/2758#issuecomment-385498560 - Thanks
+        public static Exception SafelyCatchAnNUnitException(Action code)
+        {
+            Exception caughtException = null;
+
+            using (new TestExecutionContext.IsolatedContext())
+            {
+                try
+                {
+                    code();
+                }
+                catch (Exception ex)
+                {
+                    caughtException = ex;
+                }
+
+                return caughtException;
+            }
         }
     }
 }
