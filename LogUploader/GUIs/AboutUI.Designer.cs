c4040dcc52afc6fb05b995994cbc9f76b8b60781
ProfBits <53108880+ProfBits@users.noreply.github.com>
2023-03-12 11:11:47 +0100

    Update copyright notice to 2023

--- LogUploader/GUIs/AboutUI.Designer.cs
+++ LogUploader/GUIs/AboutUI.Designer.cs
@@ -155,7 +155,7 @@
             this.lblCopyright.Name = "lblCopyright";
             this.lblCopyright.Size = new System.Drawing.Size(194, 16);
             this.lblCopyright.TabIndex = 2;
-            this.lblCopyright.Text = "© ProfBits 2019 - 2022";
+            this.lblCopyright.Text = "© ProfBits 2019 - 2023";
             // 
             // llProjectPage
             // 
